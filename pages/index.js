4bfa086c4c3e0149b3532543db0f1bb163b732b2
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 11:31:48 +0700

    map data

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import fetch from "isomorphic-fetch"
 import Error from "next/error"
+import { setPriority } from "os";
 
 class Index extends React.Component {
     static async getInitialProps () {
@@ -26,7 +27,12 @@ class Index extends React.Component {
 
         return (
             <div>
-                Hacker next
+                <h1>Hacker next</h1>
+                <div>
+                    {stories.map(story => (
+                        <h2  key={story.id}>{story.title}</h2>
+                    ))}
+                </div>
             </div>
         )
     }
