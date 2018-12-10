51f05192dc38f964ae89e9f8ce212fd3db61202e
Kittisak Pharaphan <kittisak.py@gmail.com>
2018-12-10 12:22:19 +0700

    Layout

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import Link from "next/link"
+
+const Layout = ({children}) => (
+    <div>
+        <div className="container">
+            <nav>
+                <Link href="/">
+                    <a>
+                        <span className="main-title">Hacker Next</span>
+                    </a>
+                </Link>
+            </nav>
+
+            {children}
+        </div>
+        <style jsx>{`
+            .container{
+                width: 800px;
+                margin: 0 auto;
+                background-color: #f6f6ef
+            }
+            nav{
+                background-color: #f60;
+                padding: 1em
+            }
+            nav > * {
+                display: inline-block;
+                color: black
+            }
+            nav a{
+                text-decoration: none;
+            }
+            nav .main-title{
+                font-weigth: bold
+            }
+        `}</style>
+        <style global jsx>{`
+            body{
+                background: white;
+                font-family: Vernada, Geneva, sans-serif;
+            }
+        `}</style>
+    </div>
+)
+
+export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import fetch from "isomorphic-fetch"
 import Error from "next/error"
 import StoryList from "../components/StoryList";
+import Layout from "../components/Layout"
 
 class Index extends React.Component {
     static async getInitialProps () {
@@ -26,10 +27,9 @@ class Index extends React.Component {
         }
 
         return (
-            <div>
-                <h1>Hacker next</h1>
+            <Layout>
                 <StoryList stories={stories}/>
-            </div>
+            </Layout>
         )
     }
 }
